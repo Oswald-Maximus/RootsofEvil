ed0ca231e5e5018fd1e36a16ab3bdf68e88140ef
Oswald-Maximus <tardamong@gmail.com>
2018-10-05 16:58:40 +0100

    AI Bases

---
+++ ROE/Assets/Scripts/AI/Base Minion.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMinion : MonoBehaviour {
+
+#region Stats
+public int maxHp;
+public int hp;
+public double morale;
+public double atk;
+public double build;
+public double speed;
+public int def;
+public bool friendly;
+public int range;
+#endregion
+	
+#region Functions
+public void Build()
+{
+
+}
+public void Patrol()
+{
+
+}
+public void Attack()
+{
+
+}
+public void Flee()
+{
+
+}
+public void Die()
+{
+
+}
+public void MoraleChange(int change)
+{
+	morale += change;
+}
+public void rest()
+{
+	hp++;
+}
+public void MoraleDecay()
+{
+
+}
+#endregion
+}
